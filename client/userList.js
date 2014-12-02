ffa29513f3c6aecd487e7e13cad0694a0d22e11c
ppillip <ppillip@gmail.com>
2014-12-03 01:20:30 +0900

    1
    1

---
+++ client/userList.js
@@ -0,0 +1,30 @@
+
+Template.userList.helpers({
+    listName2 : function(str,str2){
+        return str + "사용자목록 " + str2;
+    },
+    listName : function(){
+        return "사용자목록";
+    },
+    list : function(){
+
+        return Users.find( {} , {sort:{no:1}} );
+
+    }
+
+
+});
+
+Template.userList.events({
+
+});
+
+Template.userList.rendered = function(){
+
+};
+
+
+
+
+
+
